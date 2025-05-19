01809186e1b2a94e56181cbf98f7106a3aa31b4e
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-19 17:46:55 +0530

    variable practice

---
+++ Variable in python/Variables.py
@@ -0,0 +1,27 @@
+x = 5
+y = 10
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+name = "Dinesh"
+age = 20
+gender = "Male"
+
+print(name, age, gender)
+
+__ = 4
+
+___ = 10
+
+print(__+___)
+
+
+first_name = "Dinesh"
+lastName = "Kumawat"
+print(first_name + " " + lastName)  
