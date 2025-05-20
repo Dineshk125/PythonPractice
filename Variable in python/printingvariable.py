3d96be9adbc2e5e056343865af3149d31e4726bf
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-20 11:54:31 +0530

    print Variable

---
+++ Variable in python/printingvariable.py
@@ -0,0 +1,20 @@
+name = "Dinesh"
+age  = 29
+gender  = "Male"
+
+# Method 1
+print(name)
+print(age)
+print(gender)
+
+print("My Name is ", name)
+print("My Age is ", age)
+print("My Gender is ", gender)
+print("My Name is", name, "My age is",age, "my gender is", gender)
+
+# method 2
+
+print(f"my name is {name}")
+print(f"my age is {age}")   
+print(f"my gender is {gender}")
+print(f"my name is {name} my age is {age} my gender is {gender}")
