2f44d7a55c7b21ea20b1e00173dd5dd579907336
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-20 12:32:37 +0530

    type conversion and casting

---
+++ Type Conversion-/-TypeCasting/Type-Conversion-Casting.py
@@ -0,0 +1,20 @@
+'''
+# Type Conversion
+# Type Casting
+
+FROM ONE DATA TYPE TO ANOTHER
+
+# 1. int()
+# 2. float()
+# 3. str()
+# 4. bool()
+
+'''
+
+x = "100"
+y = "100.2323"
+a  = int(x)
+b = float(y)
+
+z = a+b
+print(z)
