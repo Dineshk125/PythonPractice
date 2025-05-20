b5a43ad486867226056d43fbf54402a187e8338c
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-20 12:22:47 +0530

    Data Types in Python Practice

---
+++ Data type in python/DataTypes.py
@@ -0,0 +1,76 @@
+"""Data types in python"""
+# 1. String
+# 2. Integer
+# 3. Float
+# 4. Boolean
+# 5. List
+# 6. Tuple
+# 7. Dictionary
+# 8. Set
+# 9. None
+
+#String Data type
+name = "Dinesh"
+age = 20
+is_student = True
+print(name, age, is_student)
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+
+# Integer Data type
+age = 20
+print(age)
+print(type(age))
+
+# Float Data type
+pi = 3.14
+print(pi)
+print(type(pi))
+
+# Boolean Data type
+is_student = True
+print(is_student)
+print(type(is_student))
+
+# List Data type
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+print(type(fruits))
+
+# Tuple Data type
+coordinates = (10, 20)
+print(coordinates)
+print(type(coordinates))
+
+# Dictionary Data type
+person = {"name": "Dinesh", "age": 20}
+print(person)
+print(type(person))
+
+# Set Data type
+fruits = {"apple", "banana", "cherry"}
+print(fruits)
+print(type(fruits))
+
+# None Data type
+name = None
+print(name)
+print(type(name))
+
+''' Difference between list, tuple, set and dictionary'''
+# List
+# Tuple
+# Set
+# Dictionary
+
+# List is mutable
+# Tuple is immutable
+# Set is mutable
+# Dictionary is mutable
+
+# List is ordered
+# Tuple is ordered
+# Set is unordered
+# Dictionary is unordered
