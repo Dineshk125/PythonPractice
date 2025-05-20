09ce4f7f193ed8f47566cef3140dcb6b94fd548b
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-20 12:57:46 +0530

    Use of Input Function

---
+++ Input Function/InputFunction.py
@@ -0,0 +1,11 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gender = input("Enter your Gender: ")
+print("My name is ", name)
+print("My Age is ", age)
+print("My Gender is ", gender)
+
+print(f"My name Is {name}")
+print(f"my age is {age}")
+print(f"my gender is {gender}")
