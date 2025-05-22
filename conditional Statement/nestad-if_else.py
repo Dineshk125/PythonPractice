d69f677cf831a069477e2a608b7c547dc87bb231
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-22 16:31:30 +0530

    nestad-if-else

---
+++ conditional Statement/nestad-if_else.py
@@ -0,0 +1,12 @@
+""" Q1. print the number is positive or negative by given number other than 0 """
+
+num = int(input("Enter a number: "))
+
+if num >= 0:
+    if num > 0:
+        print("The number is positive")
+    else:
+        print("The number is zero")
+
+else:
+    print("The number is negative")
