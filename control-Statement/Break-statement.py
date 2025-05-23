fae9336f3529ac528e19ab1472727bdac84a2857
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-23 14:11:30 +0530

    break statement practice

---
+++ control-Statement/Break-statement.py
@@ -0,0 +1,16 @@
+for i in range(1, 11):
+    print(i, end=" ")
+    if i == 5:
+        break
+
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i, end=" ")
+
+""" Q1. wap usinng break statement to print the number from 1 to 1000 """
+
+for i in range(1, 1001):
+    print(i, end=" ")
+    if i ==1000:
+        break
