e6d8aee11e5a914b43e3431df5c1c96e36892d75
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-23 14:20:08 +0530

    Pass statement practice

---
+++ control-Statement/pass_statement.py
@@ -0,0 +1,27 @@
+for i in range(1, 11):
+    if i == 5:
+        pass
+    print(i, end=" ")
+
+i = 1
+while i <= 10:
+    if i == 5:
+        pass
+    print(i, end=" ")
+    i = i + 1
+
+i = 1 
+
+while i <= 10:
+    if i == 5:
+        break
+    print(i, end=" ")
+    i = i + 1
+
+i = 1 
+
+while i <= 10:
+    if i == 5:
+        continue
+    print(i, end=" ")
+    i = i + 1
