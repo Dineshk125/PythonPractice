90e394d450bbb556bd35314d4608fb84d169cb83
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-24 14:07:08 +0530

    advance nested loop practice

--- Loops/advance_nested_loop.py
+++ Loops/advance_nested_loop.py
@@ -1,47 +1,108 @@
-"""
-        * 
-      * *
-    * * *
-  * * * *
-* * * * *
+# """
+#         * 
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
 
-"""
-
-
-for i in range(1,6):
-    for j in range(i,5):
-        print(" ",end=" ")
-
-    for k in range(1, i+1):
-        print("*",end=" ")
-    print()
-
-"""
-        1 
-      1 2
-    1 2 3
-  1 2 3 4
-1 2 3 4 5
-
-"""
-for i in range(1,6):
-    for j in range(i,5):
-        print(" ",end=" ")
-
-    for k in range(1, i+1):
-        print(k,end=" ")
-    print()
+# """
 
 
+# for i in range(1,6):
+#     for j in range(i,5):
+#         print(" ",end=" ")
 
-""" Logic 1
-        * 
-      * * *
-    * * * * *
-  * * * * * * *
-* * * * * * * * *
+#     for k in range(1, i+1):
+#         print("*",end=" ")
+#     print()
 
-"""
+# """
+#         1 
+#       1 2
+#     1 2 3
+#   1 2 3 4
+# 1 2 3 4 5
+
+# """
+# for i in range(1,6):
+#     for j in range(i,5):
+#         print(" ",end=" ")
+
+#     for k in range(1, i+1):
+#         print(k,end=" ")
+#     print()
+
+
+
+# """ Logic 1
+#         * 
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+# """
+# for i in range(1,10,2):
+#     for j in range(i,9):
+#         print("",end=" ")
+
+#     for k in range(1, i+1):
+#         print("*",end=" ")
+#     print()
+
+
+# """ Logic 2
+#         * 
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+# """
+
+# for i in range(1,6):
+#     for j in range(i,5):
+#         print(" ",end=" ")
+
+#     for k in range(1, i+1):
+#         print("*",end=" ")
+
+#     for l in range(1,i):
+#         print("*", end=" ")
+
+#     print()
+
+# """
+#     * 
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+# """
+
+# for i in range(1,6):
+#     for j in range(i,5):
+#         print("",end=" ")
+
+#     for k in range(1, i+1):
+#         print("*",end=" ")
+#     print()
+
+
+
+# """
+#         * 
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+#   * * * * * * *
+#     * * * * *
+#       * * *
+#         *
+
+# """
 for i in range(1,10,2):
     for j in range(i,9):
         print("",end=" ")
@@ -50,71 +111,10 @@ for i in range(1,10,2):
         print("*",end=" ")
     print()
 
-
-""" Logic 2
-        * 
-      * * *
-    * * * * *
-  * * * * * * *
-* * * * * * * * *
-
-"""
-
-for i in range(1,6):
-    for j in range(i,5):
+for m in range(4,0,-1):
+    for n in range(5,m,-1):
         print(" ",end=" ")
-
-    for k in range(1, i+1):
-        print("*",end=" ")
-
-    for l in range(1,i):
-        print("*", end=" ")
-
-    print()
-
-"""
-    * 
-   * *
-  * * *
- * * * *
-* * * * *
-
-"""
-
-for i in range(1,6):
-    for j in range(i,5):
-        print("",end=" ")
-
-    for k in range(1, i+1):
-        print("*",end=" ")
-    print()
-
-
-
-"""
-        * 
-      * * *
-    * * * * *
-  * * * * * * *
-* * * * * * * * *
-  * * * * * * *
-    * * * * *
-      * * *
-        *
-
-"""
-for i in range(1,10,2):
-    for j in range(i,9):
-        print("",end=" ")
-
-    for k in range(1, i+1):
-        print("*",end=" ")
-    print()
-
-for m in range(5,0,-1):
-    for n in range(5,m-1,-1):
-        print(" ",end=" ")
-    for o in range(1,(m*2)-2):
+    for o in range((m*2)-1):
         print("*",end=" ")
     print()
         
