aa9e3dc87776826e0a18d56c05cabb6b5ad9b1e2
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-24 13:25:15 +0530

    Advance nested loop practice

---
+++ Loops/advance_nested_loop.py
@@ -0,0 +1,120 @@
+"""
+        * 
+      * *
+    * * *
+  * * * *
+* * * * *
+
+"""
+
+
+for i in range(1,6):
+    for j in range(i,5):
+        print(" ",end=" ")
+
+    for k in range(1, i+1):
+        print("*",end=" ")
+    print()
+
+"""
+        1 
+      1 2
+    1 2 3
+  1 2 3 4
+1 2 3 4 5
+
+"""
+for i in range(1,6):
+    for j in range(i,5):
+        print(" ",end=" ")
+
+    for k in range(1, i+1):
+        print(k,end=" ")
+    print()
+
+
+
+""" Logic 1
+        * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+"""
+for i in range(1,10,2):
+    for j in range(i,9):
+        print("",end=" ")
+
+    for k in range(1, i+1):
+        print("*",end=" ")
+    print()
+
+
+""" Logic 2
+        * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+"""
+
+for i in range(1,6):
+    for j in range(i,5):
+        print(" ",end=" ")
+
+    for k in range(1, i+1):
+        print("*",end=" ")
+
+    for l in range(1,i):
+        print("*", end=" ")
+
+    print()
+
+"""
+    * 
+   * *
+  * * *
+ * * * *
+* * * * *
+
+"""
+
+for i in range(1,6):
+    for j in range(i,5):
+        print("",end=" ")
+
+    for k in range(1, i+1):
+        print("*",end=" ")
+    print()
+
+
+
+"""
+        * 
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+
+"""
+for i in range(1,10,2):
+    for j in range(i,9):
+        print("",end=" ")
+
+    for k in range(1, i+1):
+        print("*",end=" ")
+    print()
+
+for m in range(5,0,-1):
+    for n in range(5,m-1,-1):
+        print(" ",end=" ")
+    for o in range(1,(m*2)-2):
+        print("*",end=" ")
+    print()
+        
