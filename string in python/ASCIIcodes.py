718736bfd8dc7fc3f7019dd3e2f0c1291346dd26
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-27 19:08:07 +0530

    ASCII code

---
+++ string in python/ASCIIcodes.py
@@ -0,0 +1,27 @@
+# ASCII Codes
+
+first = "Dinesh"
+Second = "kumawat"
+
+print(first + " " + Second)
+print(first * 3, Second * 3)
+
+# print(first - Second)  # TypeError: unsupported operand type(s) for -: 'str' and 'str
+
+# print(first / Second) # TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+# we perform operators in String
+
+print(first < Second)
+
+ch = "A"
+ch2 = "a"
+
+# ord -> using charectar to check alphabats value,  this is show the decimal number of charectors in string
+
+print(ord(ch))
+print(ord(ch2))
+
+# chr -> using number to print alphabets
+num = 97
+print(chr(num))
