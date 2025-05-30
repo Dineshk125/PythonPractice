b4d2db320729e5a7990357b10478d434353a44d4
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-05-30 17:25:09 +0530

    write and append files

---
+++ File Handling/write-append.py
@@ -0,0 +1,12 @@
+# Write and appendMode in python
+
+# with open("write-append.txt", "w") as f:
+#     f.write("Dinesh Kumawat\n")
+#     f.write("Vinit\n")
+#     f.write("We are learning Python\n")
+
+# with open("write-append.txt", "a") as f:
+#     f.write("\n append line in file handling")
+
+
+import os
