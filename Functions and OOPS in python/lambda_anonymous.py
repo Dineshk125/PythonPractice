7c862ad5c1e7213fd61376e6255f95508f1d9601
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-03 13:26:43 +0530

    lambda function in prthon

---
+++ Functions and OOPS in python/lambda_anonymous.py
@@ -0,0 +1,38 @@
+def add(n1, n2, n3):
+    return n1 + n2 + n3
+
+
+print(add(2, 4, 6))
+
+using lambda function
+
+
+y = lambda n1, n2, n3: n1 + n2 + n3
+print(y(23, 5, 3))
+
+"""Q1. take aargument which will be a number.make a list 0 to N."""
+
+y = lambda n: [i for i in range(0, n + 1)]
+
+length = int(input("Enter a length list1: "))
+length1 = int(input("Enter a length of list2: "))
+list1 = y(length)
+list2 = y(length1)
+print(list1)
+print(list2)
+
+"""Q2. even odd number"""
+
+y = lambda n: print("Even") if n % 2 == 0 else print("Odd")
+
+y(100)
+y(9)
+
+"""Even return true else false"""
+
+y = lambda num: num % 2 == 0
+
+if y(100):
+    print("Even/ True")
+else:
+    print("Odd / False")
