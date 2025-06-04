352075adc930307a87a22a2667673e8b8f8770f8
Dinesh Kumawat <dineshkumawatdtr123@gmail.com>
2025-06-04 15:44:02 +0530

    import ane file in to another file

---
+++ Functions and OOPS in python/Oops/modules/dinesh.py
@@ -0,0 +1,10 @@
+import area
+
+from area import circle, ractangle, triangle
+from area import *  # all functio  import from area.py file
+
+print("Hello")
+
+circle(23.32)
+triangle(3, 4)
+ractangle(23, 5)
